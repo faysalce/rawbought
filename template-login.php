84d329bfbecb9a3aa06a379a733b10acdf459d11
Shekh Mostafizur Rahman <faysal.ce@gmail.com>
2020-12-14 01:40:03 +0600

    login sgn up msg and validation

--- template-login.php
+++ template-login.php
@@ -1,43 +1,48 @@
 <?php
+
 /**
-* Template name: Login page
+ * Template name: Login page
  * 
  */
-if(is_user_logged_in()){
+if (is_user_logged_in()) {
 
-wp_redirect(get_permalink(get_page_by_path('my-account')));
+    wp_redirect(get_permalink(get_page_by_path('my-account')));
 
-die;
+    die;
 }
- get_header();
+get_header();
 ?>
-     <main id="main-content">
-        <div class="has-fixed-navbar"></div>
-        <section class="section has-col-mb">
-          <div class="container-fluid container-xl-fluid">
+<main id="main-content">
+    <div class="has-fixed-navbar"></div>
+    <section class="section has-col-mb">
+        <div class="container-fluid container-xl-fluid">
             <div class="row no-gutters align-items-md-center">
                 <div class="col-md-12 mx-auto c-mb">
                     <div class="box-auth auth-login box-auth-single mx-auto">
                         <div class="auth-container">
-                        <?php if(isset($_REQUEST['reset-link-sent']) && $_REQUEST['reset-link-sent']==true){?>
-                                
-                            
-                            <div class="alert alert-default alert-dismissible fade show" role="alert">
-                        You will receive an email with a link to reset your password.
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                            
-                           <?php }?>
+
+
+
                             <div class="auth-heading">
-                                <h4 class="auth-title text-uppercase"><?php echo get_the_title();?></h4>
-                           
+                                <h4 class="auth-title text-uppercase"><?php echo get_the_title(); ?></h4>
+                                <?php if (isset($_REQUEST['reset-link-sent']) && $_REQUEST['reset-link-sent'] == true) { ?>
+
+
+                                    <div class="alert alert-default alert-dismissible fade show" role="alert">
+                                        You will receive an email with a link to reset your password.
+                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+
+                                <?php } ?>
+                                <div class="login-status-msg">
+                                    
+                                </div>
                             </div>
 
-                            
 
-                            <form id="checkout-login" method="post">  
+                            <form id="checkout-login" method="post">
                                 <div class="form-group floating-static-group floating-static-style-2">
                                     <label for="email" class="floating-label f-required">Email</label>
                                     <input type="email" id="checkout-username" name="username" class="form-control username floating-field" required="required">
@@ -45,7 +50,7 @@ die;
                                 </div>
                                 <div class="form-group floating-static-group floating-static-style-2">
                                     <label for="password" class="floating-label f-required">Password</label>
-                                    <input type="password" id="checkout-password" name="password" class="form-control password floating-field"  required="required">
+                                    <input type="password" id="checkout-password" name="password" class="form-control password floating-field" required="required">
 
                                 </div>
                                 <div class="form-group text-right">
@@ -55,7 +60,7 @@ die;
                                     <!-- <button type="submit" class="btn btn-site btn-primary rounded-0 he-rotate btn-lg btn-block" tabindex="-1">
                                         <span class="btn__text">Sign In</span>
                                     </button> -->
-                                    <?php wp_nonce_field( 'ajax-login-nonce', 'security' ); ?>
+                                    <?php wp_nonce_field('ajax-login-nonce', 'security'); ?>
 
                                     <button type="button" class="btn btn-site btn-primary rounded-0 he-rotate btn-lg btn-block checkout-login-btn">Sign In</button>
 
@@ -66,30 +71,30 @@ die;
                                 <div class="sign-or text-center"><span>Or</span></div>
                                 <div class="signsocials-wrap">
                                     <ul class="sign-socials">
-                                        <li>                                           
+                                        <li>
                                             <button class="btn btn-facebook btn-site btn-block">
-                                                <i class="fab fa-facebook-f"></i> 
+                                                <i class="fab fa-facebook-f"></i>
                                                 <span class="btn-text">Facebook</span>
                                             </button>
                                         </li>
                                         <li>
                                             <button class="btn btn-google btn-site btn-block">
-                                                <i class="fab fa-google"></i> 
+                                                <i class="fab fa-google"></i>
                                                 <span class="btn-text">Google</span>
                                             </button>
                                         </li>
                                     </ul>
-                                </div> 
+                                </div>
                                 <div class="text-center sign-bottometa">
-                                    Need to create an account? <a href="<?php echo get_permalink(get_page_by_path('signup'));?>">Sign up here.</a>
-                                </div>                              
+                                    Need to create an account? <a href="<?php echo get_permalink(get_page_by_path('signup')); ?>">Sign up here.</a>
+                                </div>
                             </form>
-                        </div>            
+                        </div>
                     </div>
                 </div>
-            </div> 
-          </div>
-        </section>
-    </main>
+            </div>
+        </div>
+    </section>
+</main>
 
-<?php get_footer();?>
+<?php get_footer(); ?>
