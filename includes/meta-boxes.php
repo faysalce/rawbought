791ea6fe09c428c64ae620415f5e8ad0eb0e4436
Shekh Mostafizur Rahman <faysal.ce@gmail.com>
2020-12-14 00:02:51 +0600

    product desc test

--- includes/meta-boxes.php
+++ includes/meta-boxes.php
@@ -6,30 +6,30 @@
 /**
  * Builds the Meta Boxes.
  */
-// add_action('admin_init', '_custom_meta_boxes');
+add_action('admin_init', '_custom_meta_boxes');
 
-// function _custom_meta_boxes() {
+function _custom_meta_boxes() {
 
-//     $meta_args_array = array(
+    $meta_args_array = array(
 
-//         array(
-//             'id' => 'return_delivery',
-//             'title' => 'return_delivery',
-//             'pages' => array('product'),
-//             'context' => 'normal',
-//             'priority' => 'high',
-//             'fields' => array(
-//                 array(
-//                     'id' => 'return_delivery',
-//                     'label' => 'Delivery and Return',
-//                     'desc' => '',
-//                     'std' => '',
-//                     'type' => 'textarea',
+        array(
+            'id' => 'return_delivery',
+            'title' => 'return_delivery',
+            'pages' => array('product'),
+            'context' => 'normal',
+            'priority' => 'high',
+            'fields' => array(
+                array(
+                    'id' => 'return_delivery',
+                    'label' => 'Delivery and Return',
+                    'desc' => '',
+                    'std' => '',
+                    'type' => 'textarea',
 
 
-//                     )
-//                 )
-//             )
+                    )
+                )
+            )
 
 
                 
@@ -38,11 +38,11 @@
 
 
 
-//                 );
+                );
 
 
-// /* load each metabox */
-// foreach ($meta_args_array as $meta_args) {
-//     ot_register_meta_box($meta_args);
-// }
-// }
+/* load each metabox */
+foreach ($meta_args_array as $meta_args) {
+    ot_register_meta_box($meta_args);
+}
+}
