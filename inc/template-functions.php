4c13f39424bd95b3a21ae137d36da34b5fd36345
Shekh Mostafizur Rahman <faysal.ce@gmail.com>
2020-12-14 05:11:08 +0600

    shipping

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -1936,3 +1936,14 @@ function save_fields_values_for_job_apply() {
 }
 
 
+function my_hide_shipping_when_free_is_available( $rates ) {
+	$free = array();
+	foreach ( $rates as $rate_id => $rate ) {
+		if ( 'free_shipping' === $rate->method_id ) {
+			$free[ $rate_id ] = $rate;
+			break;
+		}
+	}
+	return ! empty( $free ) ? $free : $rates;
+}
+add_filter( 'woocommerce_package_rates', 'my_hide_shipping_when_free_is_available', 100 );
